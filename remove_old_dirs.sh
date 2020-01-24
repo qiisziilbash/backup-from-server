7062c2902c51a8ccdce9a598857140468a9e85c3
qiisziilbash <m.shafiei0423@gmail.com>
2020-01-24 13:24:47 -0700

    remove old dirs were commented

--- remove_old_dirs.sh
+++ remove_old_dirs.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 cd /path/to/your/backup/directory
+
+# collect the directory names and put them in old_dirs.txt
+# skip the ./
 find ./ -maxdepth 1 -type d -print | sort | tail -n +2 > old_dirs.txt
+
+# find the number of directoies
 count=0
 while read p; do
 	((count=count+1))
 done < old_dirs.txt
+
 
 #keep last 2 backups
 keep=2
